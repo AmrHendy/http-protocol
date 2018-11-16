bc8a13eb9632a88a1a38c61d8ddcf07a16c759f6
Mohammed Deifallah <mohammed_deifallah@yahoo.com>
2018-11-16 16:32:28 +0200

    client side

--- src/client.c
+++ src/client.c
@@ -1,5 +1,5 @@
 //
-// Created by Amr Hendy on 11/16/2018.
+// Created by Amr Hindi on 11/16/2018.
 //
 
 #include "client.h"
@@ -10,15 +10,90 @@
 // 3) send the request using Sender
 // 4) get the response using Receiver
 // 5) execute action
+FILE *fp;
+bool file = false;
+int sock;
 
-void establishConnection(){
-
+void establishConnection() {
+    char *line = readCommand();
+    command_struct command = parse(line);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 }
 
-char* readCommand(){
-
+char *readCommand() {
+    if (fp == NULL && !file) {
+        fp = fopen(file_name, "r");
+        file = true;
+    }
+    if (fp == NULL) {
+        perror("Error while opening the file.\n");
+        file = false;
+        exit(EXIT_FAILURE);
+    }
+    char command[100];
+    int index = 0;
+    char c = fgetc(fp);
+    while (c != EOF || c != '\n') {
+        command[index++] = c;
+        c = fgetc(fp);
+    }
+    command[index] = '\0';
+    if (c == EOF) {
+        file = false;
+        fclose(fp);
+    }
+    return command;
 }
 
-void startClient(){
+void startClient(int port_number, int ip_number) {
+    setIPNumber(ip_number);
+    setPortNumber(port_number);
+}
 
+void setPortNumber(int num) {
+    port_number = num;
+}
+
+void setIPNumber(int num) {
+    ip_number = num;
+}
+
+command_struct parse(char *command) {
+    command_struct result;
+    int index = 0;
+    int i = 0;
+    char temp[100];
+    while (command[index++] != ' ') {
+        temp[i++] = command[index - 1];
+    }
+    temp[i] = '\0';
+    if(strcmp(temp, "GET")){
+        result.type = 0;
+    }
+    else{
+        result.type = 1;
+    }
+    i = 0;
+    while (command[index++] != ' ') {
+        result.file_name[i++] = command[index - 1];
+    }
+    result.file_name[i] = '\0';
+    i = 0;
+    while (command[index] != ' ' || command[index] != '\0') {
+        temp[i++] = command[index++];
+    }
+    temp[i] = '\0';
+    i = 0;
+    result.ip_number = atoi(temp);
+    if(command[index] == '\0'){
+        return result;
+    }
+    i = 0;
+    while (command[index] != '\0') {
+        temp[i++] = command[index++];
+    }
+    temp[i] = '\0';
+    i = 0;
+    result.port_number = atoi(temp);
+    return result;
 }
