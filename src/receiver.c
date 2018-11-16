b11204dde1586b77d076a329f156c4b477ddc56a
Amr Hendy <amr.m.hendy@gmail.com>
2018-11-16 15:00:16 +0200

    Added Receiver SKelton

--- src/receiver.c
+++ src/receiver.c
@@ -3,3 +3,31 @@
 //
 
 #include "receiver.h"
+
+/*
+ * Client call this function to receive response from the serevr.
+ */
+void receiveResponse(){
+
+}
+
+/*
+ * Server call this function to respond to any request from client.
+ */
+void receiveRequest(char *request, int request_size, int client_socketfd){
+
+}
+
+/*
+ * Server call this function to respond to GET request from client.
+ */
+void receiveGetRequest(char *get_request, int request_size, int client_socketfd){
+
+}
+
+/*
+ * Server call this function to respond to POST request from client.
+ */
+void receivePOSTRequest(char *get_request, int request_size, int client_socketfd){
+
+}
--- src/receiver.h
+++ src/receiver.h
@@ -5,4 +5,14 @@
 #ifndef RECEIVER_H
 #define RECEIVER_H
 
+/*
+ * Client call this function to receive response from the serevr.
+ */
+void receiveResponse();
+
+/*
+ * Server call this function to respond to any request from client.
+ */
+void receiveRequest(char *request, int request_size, int client_socketfd);
+
 #endif //RECEIVER_H
