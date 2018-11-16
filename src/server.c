7512852a239c0f49190b123e681e4e8d1301c3cc
Amr Hendy <amr.m.hendy@gmail.com>
2018-11-16 12:54:14 +0200

    Added server skelton

--- src/server.c
+++ src/server.c
@@ -3,3 +3,22 @@
 //
 
 #include "server.h"
+
+// Steps:
+// 1) init the server with the host address and port number
+// 2) start server by listening on the server socket
+// 3) if there is any connection then accept it and make a new thread handling this connection individually
+// 4) handle the connection
+
+
+void handleConnection(){
+
+}
+
+void startServer(){
+
+}
+
+void initServer(){
+
+}
--- src/server.h
+++ src/server.h
@@ -5,4 +5,7 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+void initServer();
+
+void StartServer();
 #endif //SERVER_H
